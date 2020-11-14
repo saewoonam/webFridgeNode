9fb6c5750c0d7f463564a21c5c00b73e1b3994aa
saewoonam <saewoonam@users.noreply.github.com>
2020-11-14 15:29:59 -0700

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,52 @@
+import sirv from 'sirv';
+import polka from 'polka';
+import compression from 'compression';
+import * as sapper from '@sapper/server';
+import http from 'http';
+import io from 'socket.io';
+
+const { PORT, NODE_ENV } = process.env;
+const dev = NODE_ENV === 'development';
+
+const server = http.createServer();
+
+polka({ server }) // You can also use Express
+    .use(
+        compression({ threshold: 0 }),
+        sirv('static', { dev }),
+        sapper.middleware()
+    )
+    .listen(PORT, err => {
+        if (err) console.log('error', err);
+    });
+
+let numUsers = 0;
+
+io(server).on('connection', function(socket) {
+    ++numUsers;
+    let message = 'Server: A new user has joined the chat';
+    socket.emit('user joined', { message, numUsers });
+    socket.broadcast.emit('user joined', { message, numUsers });
+
+    socket.on('message', function(msg) {
+        socket.broadcast.emit('message', msg);
+    })
+
+
+    socket.on('disconnect', function() {
+        --numUsers;
+        socket.broadcast.emit('user left', numUsers);
+    })
+
+    socket.on('user disconnect', function(name) {
+        socket.broadcast.emit('message', `Server: ${name} has left the chat.`)
+    })
+    console.log(socket);
+    setInterval(function(){ 
+        // console.log("emit interval message");
+        if(numUsers>0) socket.emit('message', 'interval message '+socket.id);
+    }, 3000);
+});
+
+
+
